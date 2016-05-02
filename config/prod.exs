72b405db24b3faa14711944775081e883017a032
Gabriel Torello <gabriel128@gmail.com>
2016-05-02 18:03:38 +1000

    Fix prod file

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,8 @@ config :facts_vs_events, FactsVsEvents.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "fact-vs-event", port: 443],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
