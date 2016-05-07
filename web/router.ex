cc8c21bbfa02347a43d11126b685557957349000
Gabriel Torello <gabriel128@gmail.com>
2016-05-07 22:13:19 +1000

    Add Plug for authentication

--- web/router.ex
+++ web/router.ex
@@ -1,3 +1,21 @@
+defmodule FactsVsEvents.Plugs.Authentication do
+  @behaviour Plug
+  import Plug.Conn
+  import Phoenix.Controller, only: [put_flash: 3, redirect: 2]
+
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
+    # if conn.assigns.current_user do
+    #   conn
+    # else
+      conn
+      |> put_flash(:error, "You must be login to access that page.")
+      |> redirect(to: "/event_user")
+      |> halt()
+    # end
+  end
+end
 defmodule FactsVsEvents.Router do
   use FactsVsEvents.Web, :router
 
@@ -9,20 +27,22 @@ defmodule FactsVsEvents.Router do
     plug :put_secure_browser_headers
   end
 
+  pipeline :authenticated do
+    plug FactsVsEvents.Plugs.Authentication, ""
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  scope "/", FactsVsEvents do
-    pipe_through :browser # Use the default browser stack
-
-    get "/", HomeController, :index
-    resources "/fact_user", FactUserController
+  scope "/users", FactsVsEvents do
+    pipe_through [:browser, :authenticated]
     resources "/event_user", EventUserController
+    resources "/fact_user", FactUserController
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", FactsVsEvents do
-  #   pipe_through :api
-  # end
+  scope "/", FactsVsEvents do
+    pipe_through :browser # Use the default browser stack
+    get "/", HomeController, :index
+  end
 end
